b0f6d01e374d2d0818296f172f1648f1d05d3a1b
franchesca <franchesca.gordon@gmail.com>
2025-10-25 05:07:07 -0400

    yay

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,15 +12,19 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  // Send JSON resoinse with formatted book data
+  res.send(JSON.stringify(books,null,4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+        // Extract the isbn parameter from the request URL
+        const isbn = req.params.isbn;
+        // Filter the book array to find books whose isbn matches the extracted isbn parameter
+        let filtered_books = books.filter((book) => book.isbn === isbn);
+        // Send the filtered_books array as the response to the client
+        res.send(filtered_books);
+    });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
